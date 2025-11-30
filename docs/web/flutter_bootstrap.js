7fd7fe82b6129796a6b4f27fce31e6f84664e429
MuhammadIrfanKhan9484 <quranraj360@gmail.com>
2025-11-30 22:04:27 +0500

    Deploy to GitHub Pages

--- docs/web/flutter_bootstrap.js
+++ docs/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3212931729"
+    serviceWorkerVersion: "2795530788"
   }
 });
