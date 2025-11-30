d6d169cf6e23b53998c7403d6d1d18a5e9830fa8
MuhammadIrfanKhan9484 <quranraj360@gmail.com>
2025-11-30 22:08:35 +0500

    Deploy to GitHub Pages

--- docs/web/flutter_service_worker.js
+++ docs/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a40e5068b9c653a04edb078cb8201836
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d0b7dd4c7f842b4187f9f8d2a7f5f443",
+"flutter_bootstrap.js": "4fad66e41436dc9081d5e956dd68cc6b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
